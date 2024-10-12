cab3067070b6de87334f58dd4a3085de54fc9c5b
parashar08 <priyanshuparashar08@gmail.com>
2024-10-12 23:02:24 +0530

    add prettierrc

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,47 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import { PrismaClient } from '@prisma/client/edge';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
-const app = new Hono()
+interface EnvBindings {
+    Bindings: {
+        DATABASE_URL: string;
+    };
+}
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const app = new Hono<EnvBindings>();
 
-export default app
+app.post('api/v1/signup', (c) => {
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate());
+
+    return c.json({
+        message: 'you created account successfully!',
+    });
+});
+
+app.post('api/v1/signin', (c) => {
+    return c.json({
+        message: 'you logged in successfully!',
+    });
+});
+
+app.post('api/v1/blog', (c) => {
+    return c.json({
+        message: 'you create blog successfully!',
+    });
+});
+
+app.put('api/v1/blod', (c) => {
+    return c.json({
+        message: 'blog updated successfully!',
+    });
+});
+
+app.get('api/v1/blog:id', (c) => {
+    return c.json({
+        message: 'here is your required blog',
+    });
+});
+
+export default app;
