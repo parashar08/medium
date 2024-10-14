9b92a367a86b4ca38371266abd203eaba77a9d3c
parashar08 <priyanshuparashar08@gmail.com>
2024-10-14 19:53:41 +0530

    write basic route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,28 +1,75 @@
 import { Hono } from 'hono';
 import { PrismaClient } from '@prisma/client/edge';
 import { withAccelerate } from '@prisma/extension-accelerate';
+import { sign, verify } from "hono/jwt"
 
 interface EnvBindings {
     Bindings: {
         DATABASE_URL: string;
+        JWT_SECRET: string
     };
 }
 
 const app = new Hono<EnvBindings>();
 
-app.post('api/v1/signup', (c) => {
+app.use('/api/v1/blog/*', async (c, next) => {
+    const header = c.req.header("Authorization") || "";
+
+    const token = header.split(" ")[1];
+
+    const response = await verify(header, c.env.JWT_SECRET)
+    if (response.id) {
+        await next();
+    } else {
+        return c.json({ error: "unauthorizd" }, 401);
+    }
+});
+
+app.post('/api/v1/signup', async (c) => {
     const prisma = new PrismaClient({
         datasourceUrl: c.env.DATABASE_URL,
     }).$extends(withAccelerate());
 
+    const body = await c.req.json();
+
+    const user = await prisma.user.create({
+        data: {
+            email: body.email,
+            password: body.password
+        }
+    })
+
+    const token = await sign({ id: user.id }, c.env.JWT_SECRET);
+
     return c.json({
-        message: 'you created account successfully!',
-    });
+        jwt: token
+    })
 });
 
-app.post('api/v1/signin', (c) => {
+app.post('/api/v1/signin', async (c) => {
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env?.DATABASE_URL
+    }).$extends(withAccelerate())
+
+    const body = await c.req.json();
+
+    const user = await prisma.user.findUnique({
+        where: {
+            email: body.email
+        }
+    });
+
+    if (!user) {
+        c.status(403);
+        return c.json({
+            error: "user not found"
+        })
+    };
+
+    const jwt = await sign({id: user.id}, c.env.JWT_SECRET)
     return c.json({
         message: 'you logged in successfully!',
+        jwt: jwt
     });
 });
 
